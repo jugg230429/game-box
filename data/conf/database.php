7e10dac5fff0785017b7e572d3fe9fa6124e4846
jugg <dvdgdhhdhhdhfjhfxh@gmail.com>
2024-08-06 21:59:48 +0800

    完善

---
+++ data/conf/database.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 配置文件
+ */
+
+return [
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => '127.0.0.1',
+    // 数据库名
+    'database' => 'db_tbb',
+    // 用户名
+    'username' => 'root',
+    // 密码
+    'password' => 'root',
+    // 端口
+    'hostport' => '3306',
+    // 数据库编码默认采用utf8
+    'charset'  => 'utf8mb4',
+    // 数据库表前缀
+    'prefix'   => 'sys_',
+    "authcode" => '5EmACrUPJF6jYDFB1e',
+    //#COOKIE_PREFIX#
+// prod
+    // 'username' => 'db_tbb',
+    // // 密码
+    // 'password' => 'LY5SmhSM8Z3j2FFJ',
+
+    //root  123456
+];
