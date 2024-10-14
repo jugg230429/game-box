2fe916e349b4cda05cd784da7c0f186cbe97f54e
jugg <dvdgdhhdhhdhfjhfxh@gmail.com>
2024-10-14 11:07:33 +0800

    配置

--- data/conf/database.php
+++ data/conf/database.php
@@ -6,14 +6,26 @@
 return [
     // 数据库类型
     'type'     => 'mysql',
+
     // 服务器地址
-    'hostname' => '127.0.0.1',
+    'hostname' => 'database-newguanwang.cn8geu6uea6i.ap-east-1.rds.amazonaws.com',
     // 数据库名
-    'database' => 'db_tbb',
+    'database' => 'tbbdb',
     // 用户名
     'username' => 'root',
     // 密码
-    'password' => 'root',
+    'password' => '{<K1+ZJC_F?HtY96R<XI0m#?R{4}',
+
+
+    // // 服务器地址
+    // 'hostname' => '127.0.0.1',
+    // // 数据库名
+    // 'database' => 'db_tbb',
+    // // 用户名
+    // 'username' => 'root',
+    // // 密码
+    // 'password' => 'root',
+    
     // 端口
     'hostport' => '3306',
     // 数据库编码默认采用utf8
@@ -21,11 +33,4 @@ return [
     // 数据库表前缀
     'prefix'   => 'sys_',
     "authcode" => '5EmACrUPJF6jYDFB1e',
-    //#COOKIE_PREFIX#
-// prod
-    // 'username' => 'db_tbb',
-    // // 密码
-    // 'password' => 'LY5SmhSM8Z3j2FFJ',
-
-    //root  123456
 ];
